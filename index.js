38b7cdca65d783624db643485c4f51fd58d1a21b
Rafarlv <rafalinaresvillareal@gmail.com>
2023-10-05 11:16:10 +0200

    areglado

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', function (req, res) {
-    res.sendFile('index.hmtl')
+    res.sendFile(path.join(__dirname,'index.hmtl'))
 })
 
 
