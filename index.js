fd346619a4a2cc3a72404cc4a90b943c47779017
Rafarlv <rafalinaresvillareal@gmail.com>
2023-10-05 10:46:21 +0200

    pagina subida

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-    res.send('index.html')
-})
+fetch('index.html')
+  .then(response => response.text())
+  .then(data => {
+    document.querySelector('body').innerHTML = data;
+  });
+
 
 
 app.listen(3000)
